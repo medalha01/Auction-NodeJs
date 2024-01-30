68513b7babadcbf694f78b270987dac145e8223c
Isac Martins <isacmartins.223@gmail.com>
2024-01-30 15:14:22 -0300

    Adding Mocks for TDD

---
+++ Backend-NestJS/src/mocks/bid.mock.ts
@@ -0,0 +1,19 @@
+export const mockBids = [
+  {
+    id: '1',
+    amount: 15000.0,
+    userId: '1',
+    auctionId: '1',
+  },
+  {
+    id: '2',
+    amount: 16000.0,
+    userId: '2',
+    auctionId: '1',
+  },
+  {
+    id: '3',
+    amount: 27000.0,
+    userId: '3',
+  },
+];
---
+++ Backend-NestJS/src/mocks/user.mock.ts
@@ -0,0 +1,42 @@
+export const mockUsers = [
+  {
+    id: '11',
+    username: 'alice123',
+    email: 'alice123@example.com',
+    password: 'password1', // In a real scenario, this would be a hashed password
+    auctions: [], // Initially empty, can be populated with mock auctions as needed
+    bids: [], // Initially empty, can be populated with mock bids as needed
+  },
+  {
+    id: '12',
+    username: 'bob456',
+    email: 'bob456@example.com',
+    password: 'password2',
+    auctions: [],
+    bids: [],
+  },
+  {
+    id: '13',
+    username: 'charlie789',
+    email: 'charlie789@example.com',
+    password: 'password3',
+    auctions: [],
+    bids: [],
+  },
+  {
+    id: '14',
+    username: 'david101',
+    email: 'david101@example.com',
+    password: 'password4',
+    auctions: [],
+    bids: [],
+  },
+  {
+    id: '15',
+    username: 'eve202',
+    email: 'eve202@example.com',
+    password: 'password5',
+    auctions: [],
+    bids: [],
+  },
+];
