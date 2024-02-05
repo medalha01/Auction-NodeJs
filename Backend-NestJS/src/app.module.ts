c870944f6a2fbb130f841ce1f8da22e8e10dda4c
Isac Martins <isacmartins.223@gmail.com>
2024-02-05 17:10:49 -0300

    improving service TODO Improve guard

--- Backend-NestJS/src/app.module.ts
+++ Backend-NestJS/src/app.module.ts
@@ -1,4 +1,4 @@
-//
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,7 +9,7 @@ import { AuctionsModule } from './auction/auction.module';
 import { APP_GUARD } from '@nestjs/core';
 import { RateLimiterModule, RateLimiterGuard } from 'nestjs-rate-limiter';
 import { JwtService } from '@nestjs/jwt';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
+
 import { ConfigModule } from '@nestjs/config';
 import { AuthService } from './auth/auth.service';
 
@@ -24,7 +24,6 @@ import { AuthService } from './auth/auth.service';
   providers: [
     AppService,
     PrismaService,
-    JwtAuthGuard,
     AuthService,
     JwtService,
     { provide: APP_GUARD, useClass: RateLimiterGuard },
--- Backend-NestJS/src/auction/auction.module.ts
+++ Backend-NestJS/src/auction/auction.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuctionsService } from './auction.service';
 import { AuctionsController } from './auction.controller';
 import { PrismaService } from '../prisma/prisma.service';
 import { JwtStrategy } from '../auth/jwt.strategy';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: 'abc161981',
-      signOptions: { expiresIn: '600s' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule], // Import ConfigModule
+      inject: [ConfigService], // Inject ConfigService
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'), // Use environment variable
+        signOptions: { expiresIn: '60m' },
+      }),
     }),
   ],
   controllers: [AuctionsController],
